9c2707c23e7a74e74a87162c210dd46ee5da2be0
SaiCodeWorld <sairam.siva2022@gmail.com>
2021-08-04 14:25:38 -0500

    Began initial prototyping of my quick sort class with basic functions to allow quick sort functionality.

--- quickSort.h
+++ quickSort.h
@@ -3,3 +3,30 @@ Name: Sai Sivakumar
 Quick Sort With Arrays C++
 Purpose: 1-3 sentences about your program.
 **********************/
+
+#ifndef QUICK_SORT_H
+#define QUICK_SORT_H
+
+#include <iostream>
+using std::cout;
+using std::cin;
+
+class QuickSort
+{
+    public:
+        QuickSort();
+        ~QuickSort();
+
+        bool sorter(int, int[]);
+        void displaySortedArr();
+
+    private:
+        int *solvedArr;
+        int arraySize;
+
+        void quickSort(int, int);
+        void partition(int, int);
+        void swap(int, int);
+};
+
+#endif //QUICK_SORT_H
